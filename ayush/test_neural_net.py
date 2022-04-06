ae819c971b90c440dcecad6da6b716694be39e1c
achakraborty <achakraborty@olin.edu>
2022-04-06 16:43:22 -0400

    implemented convolutional layer

--- ayush/test_neural_net.py
+++ ayush/test_neural_net.py
@@ -1,4 +1,9 @@
 import numpy as np
+from scipy import signal
+from keras.datasets import mnist
+from keras.utils import np_utils
+import matplotlib.pyplot as plt
+
 
 class Layer:
     """
@@ -78,6 +83,65 @@ class Tanh(Activation):
         tanh_prime = lambda x: 1 - np.tanh(x)**2
         super().__init__(tanh, tanh_prime)
 
+class Convolutional(Layer):
+    def __init__(self, input_shape, kernel_size, depth):
+        input_depth, input_height, input_width = input_shape
+        self.depth = depth
+        self.input_shape = input_shape
+        self.input_depth = input_depth
+        self.output_shape = (depth, input_height - kernel_size + 1, input_width - kernel_size + 1)
+        self.kernels_shape = (depth, input_depth, kernel_size, kernel_size)
+        self.kernels = np.random.randn(*self.kernels_shape)
+        self.biases = np.random.randn(*self.output_shape)
+    
+    def forward(self, input):
+        self.input = input
+        self.output = np.copy(self.biases)
+        for i in range(self.depth):
+            for j in range(self.input_depth):
+                self.output[i] += signal.correlate2d(self.input[j], self.kernels[i, j], 'valid')
+        return self.output
+
+    def backward(self, output_gradient, learning_rate):
+        kernels_gradient = np.zeros(self.kernels_shape)
+        input_gradient = np.zeros(self.input_shape)
+
+        for i in range(self.depth):
+            for j in range(self.input_depth):
+                kernels_gradient[i, j] = signal.correlate2d(self.input[j], output_gradient[i], 'valid')
+                input_gradient[j] += signal.convolve2d(output_gradient[i], self.kernels[i, j], 'full')
+        
+        self.kernels -= learning_rate * kernels_gradient
+        self.biases -= learning_rate * output_gradient
+        return input_gradient
+
+class Reshape(Layer):
+    def __init__(self, input_shape, output_shape):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+
+    def forward(self, input):
+        return np.reshape(input, self.output_shape)
+
+    def backward(self, output_gradient, learning_rate):
+        return np.reshape(output_gradient, self.input_shape)
+
+class Sigmoid(Activation):
+    def __init__(self):
+        def sigmoid(x):
+            return 1/(1+np.exp(-x))
+        
+        def sigmoid_prime(x):
+            s = sigmoid(x)
+            return s*(1-s)
+        super().__init__(sigmoid, sigmoid_prime)
+
+def binary_cross_entropy(y_true, y_pred):
+    return -np.mean(y_true * np.log(y_pred) + (1-y_true) * np.log(1-y_pred))
+
+def binary_cross_entropy_prime(y_true, y_pred):
+    return ((1-y_true)/(1-y_pred)-y_true/y_pred)/np.size(y_true)
+
 def mse(y_true, y_pred):
     """
     This computes the mean standard error between the predicted and actual values.
@@ -136,3 +200,71 @@ output = [[0], [1]]
 for layer in neural_net:
     output = layer.forward(output)
     print(output)
+
+## MNIST 0/1 test
+def preprocess_data(x, y, limit):
+    zero_index = np.where(y == 0)[0][:limit]
+    one_index = np.where(y == 1)[0][:limit]
+    all_indices = np.hstack((zero_index, one_index))
+    all_indices = np.random.permutation(all_indices)
+    #x, y = x[all_indices], y[all_indices]
+    x = x.reshape(x.shape[0], 1, 28, 28)
+    x = x.astype("float32")/255
+    y = np_utils.to_categorical(y)
+    y = y.reshape(y.shape[0], 10, 1)
+    return x[:limit], y[:limit]
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, y_train = preprocess_data(x_train, y_train, 2000)
+x_test, y_test = preprocess_data(x_test, y_test, 200)
+
+network = [
+    Convolutional((1, 28, 28), 3, 5),
+    Sigmoid(),
+    Reshape((5, 26, 26), (5*26*26, 1)),
+    Dense(5*26*26, 100),
+    Sigmoid(),
+    Dense(100, 10),
+    Sigmoid()
+]
+
+epochs = 60
+
+learning_rate = .1
+
+for e in range(epochs):
+    error = 0
+    for x, y in zip(x_train, y_train):
+        output = x
+        for layer in network:
+            output = layer.forward(output)
+        pred = np.argmax(output)
+        true = np.argmax(y)
+        if pred == true:
+            error += 1
+        grad = binary_cross_entropy_prime(y, output)
+
+        for layer in reversed(network):
+            grad = layer.backward(grad, learning_rate)
+
+    print(f"{e}, {error/len(x_train)}")
+# Test
+error = 0
+for x, y in zip(x_test[:-1], y_test[:-1]):
+    output = x
+    for layer in network:
+        output = layer.forward(output)
+    if np.argmax(output) == np.argmax(y):
+        error += 1
+print(f"final error: {error/len(x_test)}")
+
+plt.imshow(np.reshape(x_test[-1], (28, 28)), cmap='gray')
+
+output = x_test[-1]
+for layer in network:
+    output = layer.forward(output)
+print(f"This is number is predicted to be: {np.argmax(output)}")
+output = np.delete(output, np.argmax(output))
+print(f"Second Guess: {np.argmax(output)}")
+print(f"This number actually is: {np.argmax(y_test[-1])}")
+plt.show()
